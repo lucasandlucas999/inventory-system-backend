e70cdda075aa05fa5f4babafc48df4d822b94260
Lucas Acuna <contactolucasacuna@gmail.com>
2026-03-15 11:44:07 -0300

    feat: Implement customer management with basic role-based access control and API endpoints.

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\TestingPostController;
 use App\Http\Controllers\GetUsersController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DatabaseHealthController;
+use App\Domains\Customers\Controllers\IndexCustomersController;
 
 Route::prefix('health')->group(function () {
     Route::get('', [TestingGetController::class , '__invoke']);
@@ -19,3 +20,7 @@ Route::group(['middleware' => 'api', 'prefix' => 'auth'], function () {
     Route::post('refresh', [AuthController::class , 'refresh']);
     Route::post('me', [AuthController::class , 'me']);
 });
+
+Route::prefix('customers')->group(function () {
+    Route::get('', [IndexCustomersController::class , '__invoke']);
+}); 
