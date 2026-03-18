236ece571a25afae0245998aa3c3ab20dccf340e
Alan Quiroga <141192566+Alan-oss55@users.noreply.github.com>
2026-03-17 23:24:47 -0300

    Add Product Store endpoint

--- app/Domains/Products/Routes/api.php
+++ app/Domains/Products/Routes/api.php
@@ -4,7 +4,7 @@ use App\Domains\Products\Controllers\IndexCategoriesController;
 use App\Domains\Products\Controllers\IndexProductsController;
 use App\Domains\Products\Controllers\ShowProductController;
 use Illuminate\Support\Facades\Route;
-
+use App\Domains\Products\Controllers\StoreProductController;
 
 Route::prefix('categories')->group(function () {
     Route::get('', [IndexCategoriesController::class, '__invoke']);
@@ -17,4 +17,9 @@ Route::prefix('products')->group(function () {
 
 Route::prefix('products')->group(function () {
     Route::get('{product}', [ShowProductController::class, '__invoke']);
+});
+
+
+Route::prefix('products')->group(function () {
+    Route::post('', [StoreProductController::class, '__invoke']);
 });
